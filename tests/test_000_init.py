700694e5e45bdb16b60dae4a06928ce91efbede3
happyfaults <42094389+happyfaults@users.noreply.github.com>
2018-08-13 00:02:49 -0400

    Create test_000_init.py

---
+++ tests/test_000_init.py
@@ -0,0 +1,37 @@
+
+import sys
+from os import path
+
+# add project src dir to path
+# as second highest priority after .
+test_dir = path.dirname(__file__)
+
+src_dir = path.join(
+    path.dirname(test_dir),
+    'src'
+)
+
+conf_dir = path.join(
+    path.dirname(test_dir),
+    'config'
+)
+
+sys.path.insert(
+    1,
+    src_dir
+)
+
+sys.path.insert(
+    2,
+    conf_dir
+)
+
+import watdafudge
+import watdafudge_c
+
+import pytest
+watdafudge.pytest = pytest
+
+def test_init(caplog):
+    import sys, logging
+    logging.getLogger().info(f'Path: {sys.path}')
