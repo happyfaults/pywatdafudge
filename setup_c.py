efecf658c840347dfccd591f5777e14bb73553f8
felix_culpae <happyfaults@gmail.com>
2018-08-12 22:41:45 -0400

    Added setup.py for src/watdafudge and setup_c.py for config/watdafudge. Updated .travis.yml to run python setup*.py install

---
+++ setup_c.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+setup(
+    name='pywatdafudge_c',
+    version='0.0.2a',
+    license='MIT License',
+    description='The default config modules for pywatdafudge.',
+    author='felix',
+    author_email='happyfaults@gmail.com',
+    url='https://github.com/happyfaults/pywatdafudge',
+    packages=find_packages('config'),
+    package_dir={'': 'config'},
+    py_modules=[splitext(basename(path))[0] for path in glob('config/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+)
